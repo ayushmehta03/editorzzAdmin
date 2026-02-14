041c7b072fde0e111c2d78172384446bdedee7e2
Ayush Mehta <ayushmehtabps@gmail.com>
2026-02-14 23:41:17 +0530

    add login route

--- backend/internal/controllers/auth_controller.go
+++ backend/internal/controllers/auth_controller.go
@@ -9,8 +9,10 @@ import (
 
 	"github.com/ayushmehta03/editorzzAdmin/internal/database"
 	"github.com/ayushmehta03/editorzzAdmin/internal/models"
+	"github.com/ayushmehta03/editorzzAdmin/internal/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -95,7 +97,64 @@ func HashPassword(password string)(string,error){
 
 func Login(client *mongo.Client)gin.HandlerFunc{
 	return func(c *gin.Context){
-		
+
+		var req struct{
+			Identifier string `json:"identifier"`
+			Password string `json:"password"`
+
+		}
+
+		if err:=c.ShouldBindJSON(&req);err!=nil{
+			c.JSON(http.StatusBadRequest,gin.H{"error":"Invalid input"})
+			return 
+		}
+
+
+		ctx,cancel:=context.WithTimeout(context.Background(),10*time.Second)
+
+		defer cancel()
+
+
+		adminCollection:=database.OpenCollection("admin",client)
+
+
+		var admin models.Admin
+
+
+		filter:=bson.M{
+			"$or":[]bson.M{
+			{"email":req.Identifier},
+			{"username":req.Identifier},
+			},
+		}
+
+		if err:=adminCollection.FindOne(ctx,filter).Decode(&admin);err!=nil{
+			c.JSON(http.StatusNotFound,gin.H{"error":"No such user found"})
+			return 
+		}
+
+
+		if err:=bcrypt.CompareHashAndPassword(
+			[]byte(admin.Hashedpassword),
+			[]byte(req.Password),
+		);err!=nil{
+			c.JSON(http.StatusUnauthorized,gin.H{"error":"Wrong email or password"})
+			return 
+		}
+
+		token,err:=utils.GenerateToken(admin.Id.Hex(),admin.Email,admin.Role)
+
+		if err!=nil{
+			c.JSON(http.StatusInternalServerError,gin.H{"error":"Unable to login now"})
+			return 
+		}
+
+		c.JSON(http.StatusOK,gin.H{
+			"message":"Login Successful",
+			"token":token,
+		})
+	
+
 
 	}
 }

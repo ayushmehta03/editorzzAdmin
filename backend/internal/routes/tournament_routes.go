60d8d5110213c279f15487072621ae6b9eca0dc1
Ayush Mehta <ayushmehtabps@gmail.com>
2026-02-16 17:44:14 +0530

    route add

---
+++ backend/internal/routes/tournament_routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/ayushmehta03/editorzzAdmin/internal/controllers"
+	"github.com/ayushmehta03/editorzzAdmin/internal/middleware"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func ProtectedRoutes(router *gin.Engine,client *mongo.Client){
+
+	protected:=router.Group("/api/admin")
+
+	protected.Use(middleware.AuthMiddleWare())
+
+
+	protected.POST("/createtournament",controllers.CreateTournament(client))
+}

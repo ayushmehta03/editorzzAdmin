7fb4cbdb4b25231e1d664ff085be154e94e610af
Ayush Mehta <ayushmehtabps@gmail.com>
2026-02-16 23:12:34 +0530

    add get upcoming t api

--- backend/internal/controllers/tournamnet_controller.go
+++ backend/internal/controllers/tournamnet_controller.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 
@@ -48,3 +49,43 @@ func GetActiveTournament(client *mongo.Client)gin.HandlerFunc{
 	}
 }
 
+func GetUpcomingTournaments(client*mongo.Client)gin.HandlerFunc{
+	return func(c *gin.Context){
+
+
+		ctx,cancel:=context.WithTimeout(context.Background(),10*time.Second)
+
+		defer cancel()
+
+
+		collection:=database.OpenCollection("tournaments",client)
+
+		opts := options.Find().SetSort(bson.D{{"start_time", 1}})
+		
+
+		cursor,err:=collection.Find(ctx,bson.M{
+			"status":models.TournamentUpcoming,
+		},opts)
+
+		if err!=nil{
+			c.JSON(http.StatusInternalServerError,gin.H{"error":"Failed to fetch tournaments"})
+			return 
+		}
+
+		defer cursor.Close(ctx)
+
+		var tournaments []models.Tournament
+
+
+		if err=cursor.All(ctx,&tournaments);err!=nil{
+			c.JSON(http.StatusInternalServerError,gin.H{"error":"Failed to decode tournamnets"})
+			return 
+		}
+
+
+		c.JSON(http.StatusOK, gin.H{
+			"tournaments": tournaments,
+		})
+
+	}
+}

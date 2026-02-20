360d52fb9cf65abcdc6c69fcc2788756f310e869
Ayush Mehta <ayushmehtabps@gmail.com>
2026-02-21 01:23:17 +0530

    user side update

--- backend/internal/routes/user_routes.go
+++ backend/internal/routes/user_routes.go
@@ -7,14 +7,15 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func UserRoutes(router *gin.Engine,client *mongo.Client){
+func UserRoutes(router *gin.Engine, client *mongo.Client) {
 
-	protected:=router.Group("/api/admin")
+	admin := router.Group("/api/admin")
+	admin.Use(middleware.AuthMiddleWare())
+	admin.Use(middleware.AdminOnly())
 
-	protected.Use(middleware.AuthMiddleWare())
-	protected.Use(middleware.AdminOnly())
+	admin.GET("/users", controllers.GetAllUsers(client))
+	admin.GET("/users/search", controllers.SearchUsersByUsername(client))
 
-
-	protected.POST("/createtournament",controllers.CreateTournament(client))
-	
+	admin.PATCH("/users/:id/ban", controllers.UpdateUserBan(client))
+	admin.PATCH("/users/:id/hiring", controllers.UpdateHiringStatus(client))
 }

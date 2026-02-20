af90cac8f03a3ce59356d80e25b3e67a1f4a37f1
Ayush Mehta <ayushmehtabps@gmail.com>
2026-02-21 01:05:07 +0530

    add other two api

--- backend/internal/controllers/user_admincontroller.go
+++ backend/internal/controllers/user_admincontroller.go
@@ -10,6 +10,7 @@ import (
 	"github.com/ayushmehta03/editorzzAdmin/internal/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -84,3 +85,95 @@ func GetAllUsers(client *mongo.Client) gin.HandlerFunc {
 }
 
 
+
+func UpdateUserBan(client *mongo.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		userID := c.Param("id")
+
+		objectID, err := primitive.ObjectIDFromHex(userID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user id"})
+			return
+		}
+
+		var body struct {
+			Ban bool `json:"ban"`
+		}
+
+		if err := c.ShouldBindJSON(&body); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+			return
+		}
+
+		editorCol := database.OpenCollection("editors", client)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		_, err = editorCol.UpdateOne(
+			ctx,
+			bson.M{"_id": objectID},
+			bson.M{"$set": bson.M{
+				"ban":        body.Ban,
+				"updated_at": time.Now(),
+			}},
+		)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update ban status"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Ban status updated successfully",
+		})
+	}
+}
+
+
+
+func UpdateHiringStatus(client *mongo.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		userID := c.Param("id")
+
+		objectID, err := primitive.ObjectIDFromHex(userID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user id"})
+			return
+		}
+
+		var body struct {
+			IsHiringListed bool `json:"is_hiring_listed"`
+		}
+
+		if err := c.ShouldBindJSON(&body); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+			return
+		}
+
+		editorCol := database.OpenCollection("editors", client)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		_, err = editorCol.UpdateOne(
+			ctx,
+			bson.M{"_id": objectID},
+			bson.M{"$set": bson.M{
+				"is_hiring_listed": body.IsHiringListed,
+				"updated_at":       time.Now(),
+			}},
+		)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update hiring status"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hiring status updated successfully",
+		})
+	}
+}

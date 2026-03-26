43a6f6516cd010f46fc77b885a69bdaf4815cc70
Ayush Mehta <ayushmehtabps@gmail.com>
2026-03-26 12:42:34 +0530

    add claudinary api

---
+++ client/editorzzz-adminc/lib/claudinary.ts
@@ -0,0 +1,24 @@
+export async function uploadProfileImage(file: File) {
+  const formData = new FormData();
+
+  formData.append("file", file);
+  formData.append("upload_preset", "contest_banner");
+  formData.append("folder", "banner_images");
+
+  const res = await fetch(
+    "https://api.cloudinary.com/v1_1/dborefq7z/image/upload",
+    {
+      method: "POST",
+      body: formData
+    }
+  );
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    console.error("Cloudinary Upload Error:", data);
+    throw new Error(data?.error?.message || "Upload failed");
+  }
+
+  return data.secure_url;
+}

172b7ebd37f5efa12b5a2e3a789fa50f6553c5c8
Ayush Mehta <ayushmehtabps@gmail.com>
2026-04-11 20:03:26 +0530

    setup basic route for vote

--- backend/internal/routes/vote_routes.go
+++ backend/internal/routes/vote_routes.go
@@ -1,5 +1,19 @@
 package routes
 
-func VoteRoutes(){
+import (
+	"github.com/ayushmehta03/editorzzAdmin/internal/middleware"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
+func VoteRoutes(router *gin.Engine,client*mongo.Client){
+
+	voteR:=router.Group("/api/admin/voteC")
+
+	voteR.Use(middleware.AuthMiddleWare())
+	voteR.Use(middleware.AdminOnly())
+
+	
+
+ 
 }

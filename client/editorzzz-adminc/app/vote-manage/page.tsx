e795914171efbe7fb23192c56ab69c2680840386
Ayush Mehta <ayushmehtabps@gmail.com>
2026-06-05 14:52:51 +0530

    add updates

--- client/editorzzz-adminc/app/vote-manage/page.tsx
+++ client/editorzzz-adminc/app/vote-manage/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import { getVoteTournaments, getTotalCastedVotes } from "@/lib/api";
+import { getVoteTournaments, getTotalCastedVotes,calculateVoteScore} from "@/lib/api";
 
 type Tournament = {
   _id: string;

e25d86f44e91c1e0fcb42cdd3a055b9a4224f20a
Ayush Mehta <ayushmehtabps@gmail.com>
2026-07-10 08:27:50 +0530

    update get users api limit

--- backend/internal/controllers/user_admincontroller.go
+++ backend/internal/controllers/user_admincontroller.go
@@ -20,7 +20,7 @@ func GetAllUsers(client *mongo.Client) gin.HandlerFunc {
 	return func(c *gin.Context) {
 
 		pageStr := c.DefaultQuery("page", "1")
-		limitStr := c.DefaultQuery("limit", "10")
+		limitStr := c.DefaultQuery("limit", "20")
 
 		page, _ := strconv.Atoi(pageStr)
 		limit, _ := strconv.Atoi(limitStr)
@@ -29,7 +29,7 @@ func GetAllUsers(client *mongo.Client) gin.HandlerFunc {
 			page = 1
 		}
 		if limit < 1 {
-			limit = 10
+			limit = 20
 		}
 
 		skip := (page - 1) * limit
